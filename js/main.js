83e313cda446b3a6c70f48f4e7aeed61bbec7a9f
hidouche ait youcef <hidouche.aityoucef@gmail.com>
2021-04-17 17:38:30 +0200

    declare variables

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
-// créer un tableau
+// Declare variables 
 const colors = ["blue", "pink" ,"Yellow","Black", "pink", "Maroon","Black", "Yellow","Purple", "Blue","Maroon", "Purple"];
-
 let openedCards = [];
-
 let allCards = document.getElementsByClassName("colorCard");
-
-// random cards
-  colors.sort(function (a, b) {
-    return 0.5 - Math.random ()
-  });
-  
-
-// fonction pour changer la couleur  au clique
-for (let i = 0; i < allCards.length; i++){
- 
-    allCards[i].onclick = function(){
-     allCards[i].style.backgroundColor = colors[i];
-     openedCards.push(allCards[i]);
-  }
-  
-}
+let score = document.getElementById("score");
+score = 0;
 
