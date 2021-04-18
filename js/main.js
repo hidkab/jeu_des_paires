ad0a249492b5c5b25bdb59e3314a5e38d3284b0a
hidouche ait youcef <hidouche.aityoucef@gmail.com>
2021-04-18 19:56:00 +0200

    Functions js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Declare variables 
-const colors = ["blue", "pink" ,"Yellow","Black", "pink", "Maroon","Black", "Yellow","Purple", "Blue","Maroon", "Purple"];
+const colors = ["blue", "pink" ,"green","Black", "pink", "Maroon","Black", "green","Purple", "Blue","Maroon", "Purple"];
 let openedCards = [];
 let allCards = document.getElementsByClassName("colorCard");
-let score = document.getElementById("score");
+let resultScore = document.getElementById("score");
 score = 0;
 
 // Random cards
@@ -10,12 +10,42 @@ colors.sort(function (a, b) {
   return 0.5 - Math.random ()
 });
 
-// Loop to change the color of cards on click
-for (let i = 0; i < allCards.length; i++){
-  allCards[i].onclick = function(){
-   allCards[i].style.backgroundColor = colors[i];
-   openedCards.push(allCards[i]);
+// functions
+// Reset cardes not mached 
+function resetClick(){
+  if(openedCards.length > 2 ) {
+      //remet le click sur l'élément 
+      openedCards[0].style.pointerEvents = "auto";
+      openedCards[0].style.backgroundColor = "#DBA901";
+      openedCards[1].style.backgroundColor = "#DBA901";
+      openedCards[2].style.backgroundColor = "#DBA901";
+      openedCards = [];
+  }  
+}
+
+// add opened cards to OpenedCards list and check if cards are match or not
+function compare(){
+  if (window.getComputedStyle(openedCards[0],null).getPropertyValue("background") === window.getComputedStyle(openedCards[1],null).getPropertyValue("background")) {        
+      score ++;
+      resultScore.innerText = "Score = " + score; 
+      endGame();      
+  }
+  else {
+      openedCards[0].style.background = "#DBA901";
+      openedCards[1].style.background = "#DBA901";
+
+  }
+  openedCards[0].style.pointerEvents = "auto";
+  openedCards = [];
+}
+
+// If score equal 6
+function endGame(){
+  if (score === 6){
+    alert("Bravo vous avez gagné");
   }
 }
+
+
 
 
